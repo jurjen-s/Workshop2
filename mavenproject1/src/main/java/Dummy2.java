0f499b84d4dc9c0a44635c95a425d92ea6ffbf67
FRANK <lorus1@live.nl>
2017-02-28 13:06:34 +0100

    Dummy2 gemaakt

---
+++ mavenproject1/src/main/java/Dummy2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frank
+ */
+public class Dummy2 {
+    
+    
+    private final int Simon = 5986;
+}
